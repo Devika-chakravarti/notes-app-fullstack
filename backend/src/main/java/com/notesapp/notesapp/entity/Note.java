7b7190c3b5476048fb308de85ed3006e0b69c5d1
Devika-Chakravarti <devikachakravarti4@gmail.com>
2026-08-02 12:40:51 +0530

    [Devika] : Modify Note.java

--- backend/src/main/java/com/notesapp/notesapp/entity/Note.java
+++ backend/src/main/java/com/notesapp/notesapp/entity/Note.java
@@ -2,11 +2,14 @@ package com.notesapp.notesapp.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,18 +18,22 @@ import lombok.Setter;
 @Setter
 public class Note {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String title;
+    @NotBlank(message = "Title cannot be empty")
+    @Size(max = 100, message = "Title cannot exceed 100 characters")
+    private String title;
 
-	private String content;
+    @NotBlank(message = "Content cannot be empty")
+    @Column(columnDefinition = "TEXT")
+    private String content;
 
-	private LocalDateTime createdDate;
+    private LocalDateTime createdDate;
 
-	@PrePersist
-	protected void onCreate() {
-		this.createdDate = LocalDateTime.now();
-	}
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
